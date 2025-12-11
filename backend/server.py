761db898cfeb5f5a84e032778c6ab184b44fd00d
kabirgogia007 <kabirgogia7306@gmail.com>
2025-12-12 01:17:27 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -118,5 +118,5 @@ def get_eda_image(image_name: str):
     
     return FileResponse(image_name)
 
-if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+#if __name__ == "__main__":
+#  uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
